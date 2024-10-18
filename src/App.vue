4d1005d0e8e7bc1ff0c299f8a48a2e769ef55202
RuslanGon <ruslan441006@gmail.com>
2024-10-18 15:09:06 +0300

    wer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
-<script>
-
-</script>
 
 <template>
-  <h1>Hello Ruslan</h1>
+<h3>{{ info }}</h3>
+<p>{{ someInfo }}</p>
+<button type="button" @click="this.info = 'some info'">Send</button>
+
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      info: 'Title',
+      someInfo: "Anons message"
+    }
+  }
+}
+</script>
+
 <style scoped>
-h1 {
+
+h3 {
+  font-size: 24px;
+  font-weight: lighter;
   color: gold;
+}
+p {
+font-size: 20px;
 }
 
 </style>
