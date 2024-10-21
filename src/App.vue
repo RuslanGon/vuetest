9a677b41a15d68f080bbb9b79b60c88c7d789d1f
RuslanGon <ruslan441006@gmail.com>
2024-10-21 17:29:26 +0300

    wer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper">
-    <h1>Wather aplication</h1>
+    <h1>Weather Application</h1>
   </div>
 </template>
 
@@ -10,5 +10,14 @@
 
 
 <style scoped>
-
+.wrapper {
+  width: 300px; /* Увеличил ширину для лучшего отображения */
+  height: 300px; /* Увеличил высоту */
+  background-color: brown;
+  display: flex; /* Добавил flex для центрирования содержимого */
+  justify-content: center; /* Центр по горизонтали */
+  align-items: center; /* Центр по вертикали */
+  color: white; /* Добавил белый цвет текста для контраста */
+  border: 2px solid black; /* Добавил рамку для лучшего визуального восприятия */
+}
 </style>
