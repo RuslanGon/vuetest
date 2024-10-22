c7bf5b89ce7b0e6affd33198d498cbd3ec17747a
RuslanGon <ruslan441006@gmail.com>
2024-10-22 15:35:55 +0300

    wer

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -13,4 +13,25 @@ export default {
 </script>
 
 <style scoped>
+ul {
+    list-style: none;
+    width: 200px;
+    padding: 0;
+    border-radius: 3px;
+    border: 3px solid gold;
+    margin-left: 540px;
+    margin-top: 20px;
+}
+
+li {
+    display: block;
+    background-color: transparent;
+    padding: 20px 0;
+}
+
+li:hover, li:active {
+    cursor: pointer;
+    background-color: rgb(253, 242, 180);
+    color: black;
+}
 </style>
