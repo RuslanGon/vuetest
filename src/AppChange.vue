f02410d5988f567377320566b0d6a7bb7670734c
RuslanGon <ruslan441006@gmail.com>
2024-10-22 15:26:58 +0300

    wer

--- src/AppChange.vue
+++ src/AppChange.vue
@@ -1,12 +1,17 @@
 <template>
   <h1>CRIPTO</h1>
   <Input/>
+  <Selector />
   </template>
   
   <script>
  import Input from './components/Input.vue';
+ import Selector from './components/Selector.vue';
+
+
+
  export default {
-  components: {Input},
+  components: {Input, Selector},
  }
   </script>
   
---
+++ src/components/Selector.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul>
+        <li>Bitcoin</li>
+        <li>ETH</li>
+        <li>USDT</li>
+    </ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+</style>
