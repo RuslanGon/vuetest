15d48f86476585cfc896d1c1ef2ebd0fee086d5d
RuslanGon <ruslan441006@gmail.com>
2024-10-22 16:24:08 +0300

    wer

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -1,54 +1,58 @@
 <template>
     <ul>
-        <li @click="selectItem('BTC')" :class="current == 'BTC' ? 'active' : ''">Bitcoin</li>
-        <li @click="selectItem('ENT')" :class="current == 'ENT' ? 'active' : ''">ENT</li>
-        <li @click="selectItem('USDT')" :class="current == 'USDT' ? 'active' : ''">USDT</li>
+      <li @click="selectItem('BTC')" :class="current === 'BTC' ? 'active' : ''">Bitcoin</li>
+      <li @click="selectItem('ETH')" :class="current === 'ETH' ? 'active' : ''">ENT</li>
+      <li @click="selectItem('USDT')" :class="current === 'USDT' ? 'active' : ''">USDT</li>
     </ul>
-</template>
-
-<script>
-export default {
-props: {
-    setCrypto: {
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      setCrypto: {
         type: Function,
         required: true
+      }
+    },
+    data() {
+      return {
+        current: ''  
+      }
+    },
+    methods: {
+      selectItem(val) {
+        this.setCrypto(val);  
+        this.current = val;   
+      }
     }
-},
-data() {
-    return {
-        current: ''
-    }
-},
-methods: {
-selectItem(val) {
-    this.setCrypto(val),
-    this.current = val
-}
-}
-}
-</script>
-
-<style scoped>
-ul {
+  }
+  </script>
+  
+  <style scoped>
+  ul {
     list-style: none;
     width: 170px;
     padding: 0;
     border-radius: 3px;
     border: 3px solid gold;
     margin-top: 20px;
-}
-
-li {
+  }
+  
+  li {
     display: block;
     background-color: transparent;
     padding: 20px 0;
-}
-
-li:hover, li:active {
+  }
+  
+  li:hover, li:active {
     cursor: pointer;
     background-color: rgb(253, 242, 180);
     color: black;
-}
-
-
-</style>
+  }
+  
+  .active {
+    background-color: gold;  
+    color: black;
+  }
+  </style>
+  
