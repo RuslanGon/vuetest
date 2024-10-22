d85a142f20b79ecad49e76643a030bb49dd708e9
RuslanGon <ruslan441006@gmail.com>
2024-10-22 15:02:58 +0300

    wer

--- src/AppChange.vue
+++ src/AppChange.vue
@@ -1,9 +1,13 @@
 <template>
   <h1>CRIPTO</h1>
+  <Input/>
   </template>
   
   <script>
- 
+ import Input from './components/Input.vue';
+ export default {
+  components: {Input},
+ }
   </script>
   
   <style scoped>
--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,11 +1,20 @@
 <template>
-
+<input type="number">
 </template>
 
 <script>
+export default {
 
+}
 </script>
 
 <style scoped>
-
+input {
+    outline: none;
+    border-radius: 7px;
+    border: 0;
+    padding: 10px 15px;
+    background-color: aliceblue;
+    font-size: 24px;
+}
 </style>
