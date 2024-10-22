1fde054adbbc1e1dee34b499da30c924ad01ba2e
RuslanGon <ruslan441006@gmail.com>
2024-10-22 16:11:29 +0300

    wer

--- src/AppChange.vue
+++ src/AppChange.vue
@@ -1,27 +1,37 @@
 <template>
-  <h1>CRIPTO</h1>
-  <Input :changeAmount="changeAmount"/>
-  {{ amount }}
-  <div class="selectors">
-    <Selector />
-    <Selector />
+  <div>
+    <h1>CRYPTO</h1>
+    <Input :changeAmount="changeAmount"/>
+     {{ cryptoFirst }} и {{ cryptoSecond }}
+    <div class="selectors">
+      <Selector :setCrypto="setCryptoFirst"/>
+      <Selector :setCrypto="setCryptoSecond"/>
+    </div>
   </div>
-
 </template>
 
 <script>
 import Input from './components/Input.vue';
 import Selector from './components/Selector.vue';
+
 export default {
   components: { Input, Selector },
   data() {
     return {
-      amount: 0
+      amount: 0,
+      cryptoFirst: '',   
+      cryptoSecond: ''  
     }
   },
   methods: {
     changeAmount(val) {
-      this.amount = val
+      this.amount = val;
+    },
+    setCryptoFirst(val) {
+      this.cryptoFirst = val;
+    },
+    setCryptoSecond(val) {
+      this.cryptoSecond = val;
     }
   }
 }
@@ -30,9 +40,7 @@ export default {
 <style scoped>
 .selectors {
   display: flex;
-margin-left: 440px;
-gap: 20px;
-
-
+  margin-left: 440px;
+  gap: 20px;
 }
 </style>
--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -1,14 +1,19 @@
 <template>
     <ul>
-        <li>Bitcoin</li>
-        <li>ETH</li>
-        <li>USDT</li>
+        <li @click="setCrypto('BTC')">Bitcoin</li>
+        <li @click="setCrypto('ENT')">ETH</li>
+        <li @click="setCrypto('USDT')">USDT</li>
     </ul>
 </template>
 
 <script>
 export default {
-
+props: {
+    setCrypto: {
+        type: Function,
+        required: true
+    }
+}
 }
 </script>
 
